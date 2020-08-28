b7b103609c1368d9a74c755bdc1ab4ea3b6183fd
Artem <Artem@DESKTOP-JJ9O5GM>
2020-08-28 16:23:03 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    //Настройка Redux Devtools
+    StoreDevtoolsModule.instrument({
+      //Макс. количество экшенов
+      maxAge: 25,
+      //Делаем лог только для чтения на продакшн                     
+      logOnly: environment.production 
+    }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
